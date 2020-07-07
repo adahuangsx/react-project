3a1c03835bcc55b0d4840ec36712320103f739f8
adahuangsx <huangsx_97@foxmail.com>
2020-07-07 00:45:20 -0400

    "last search" and a counter

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
     
     const [recipes, setRecipes] = useState([]);
     const [keyword, setKeyword] = useState('');
+    const [lastKeyword, setLastKeyword] = useState('chicken');
     const [query, setQuery] = useState('chicken'); // by default "chicken"
     const exampleReq = 
         `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}`;
@@ -23,6 +24,7 @@ const App = () => {
     const search = (e) => {
         e.preventDefault();
         setQuery(keyword);
+        setLastKeyword('Last search: ' + keyword);
         setKeyword('');  // reset the input
     }
 
@@ -44,10 +46,11 @@ const App = () => {
         <div>
             <form onSubmit={search} action="" className="search-form">
                 <input type="text" className="search-bar" value={keyword} 
-                        onChange={updateKeyword} placeholder="chicken"/>
+                        onChange={updateKeyword} placeholder={lastKeyword}/>
                 <button type="submit" className="search-button">search</button>
                 {/* Note: these functions are "const", so no paratheses.~ */}
             </form>
+    <p>{recipes.length} recipes are found: </p>
         </div>
         <div className="recipes">
             {recipes.map(recipe => (
